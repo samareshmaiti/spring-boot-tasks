4c757780d0790e3f3706336462d6021327969dfe
Samaresh Maiti <38136545+samareshmaiti@users.noreply.github.com>
2019-08-05 21:34:47 +0530

    Update TrackRepository.java

--- src/main/java/com/stackroute/repository/TrackRepository.java
+++ src/main/java/com/stackroute/repository/TrackRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+//The central interface in Spring Data repository abstraction is Repository.
+// It takes the the domain class to manage as well as the id type of the domain class as type arguments.
 
 @Repository
 public interface TrackRepository extends JpaRepository<Track, Integer> {
+    
 
     @Query("select track from Track track where track.name like ?1")
     List<Track> getTrackByName(String name);
