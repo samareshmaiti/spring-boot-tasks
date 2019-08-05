e9ccf86dd64f5482dc5c6b02e71453e60c66be88
samaresh.maiti <samaresh.maiti@cgi.com>
2019-08-05 21:25:36 +0530

    adding comment to TRackRepository class

--- src/main/java/com/stackroute/repository/TrackRepository.java
+++ src/main/java/com/stackroute/repository/TrackRepository.java
@@ -1,16 +1,16 @@
 package com.stackroute.repository;
-
 import com.stackroute.domain.Track;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
+
+//The central interface in Spring Data repository abstraction is Repository.
+// It takes the the domain class to manage as well as the id type of the domain class as type arguments.
 @Repository
 public interface TrackRepository extends MongoRepository<Track, Integer> {
 
     List<Track> getTrackByName(String name);
+
 
 }
