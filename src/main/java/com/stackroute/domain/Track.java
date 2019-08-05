58923d77ebac6561eb3ecbeb929848988ae78315
samaresh.maiti <samaresh.maiti@cgi.com>
2019-08-05 20:59:56 +0530

    implementing lombok

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,10 +1,12 @@
 package com.stackroute.domain;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 @Document
 @Data
 @AllArgsConstructor
