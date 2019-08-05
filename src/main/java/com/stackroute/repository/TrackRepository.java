98723c6b2c06a16ffa9490e1cd4bec7947e85c05
Samaresh Maiti <38136545+samareshmaiti@users.noreply.github.com>
2019-08-05 21:30:28 +0530

    Update TrackRepository.java

--- src/main/java/com/stackroute/repository/TrackRepository.java
+++ src/main/java/com/stackroute/repository/TrackRepository.java
@@ -4,6 +4,8 @@ import com.stackroute.domain.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//The central interface in Spring Data repository abstraction is Repository.
+// It takes the the domain class to manage as well as the id type of the domain class as type arguments.
 @Repository
 public interface TrackRepository extends JpaRepository<Track, Integer> {
 
