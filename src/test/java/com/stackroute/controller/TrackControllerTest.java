f3e4745b9ca7284d776b3e8e66cea66f47a51aaf
samaresh.maiti <samaresh.maiti@cgi.com>
2019-08-05 22:09:49 +0530

    implementing test cases

--- src/test/java/com/stackroute/controller/TrackControllerTest.java
+++ src/test/java/com/stackroute/controller/TrackControllerTest.java
@@ -1,7 +1,98 @@
 package com.stackroute.controller;
 
-import static org.junit.Assert.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stackroute.domain.Track;
+import com.stackroute.exceptions.TrackAlreadyExistsException;
+import com.stackroute.service.TrackService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.BasicJsonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class TrackControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    private Track track;
+    @MockBean
+    private TrackService trackService;
+    @InjectMocks
+    private TrackController trackController;
+
+    private List<Track> list = null;
+
+    @Before
+    public void setUp() {
+
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(trackController).build();
+        track = new Track();
+        track.setId(10);
+        track.setName("track name");
+        track.setComment("comment1");
+        list = new ArrayList();
+
+        list.add(track);
+    }
+
+    @Test
+    public void saveTrack() throws Exception {
+        when(trackService.saveTrack(any())).thenReturn(track);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/")
+                .contentType(MediaType.APPLICATION_JSON).content(asJsonString(track)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+
+
+    }
+
+    @Test
+    public void saveTrackFaliure() throws Exception {
+        when(trackService.saveTrack(any())).thenThrow(TrackAlreadyExistsException.class);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/")
+                .contentType(MediaType.APPLICATION_JSON).content(asJsonString(track)))
+                .andExpect(MockMvcResultMatchers.status().isConflict())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void getAllTrack() throws Exception {
+        when(trackService.getAllTracks()).thenReturn(list);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/")
+                .contentType(MediaType.APPLICATION_JSON).content(asJsonString(track)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
