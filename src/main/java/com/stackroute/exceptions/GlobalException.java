0bf6b8217d4e34c1f7e0a28892ca1d76dcbf681c
Samaresh Maiti <38136545+samareshmaiti@users.noreply.github.com>
2019-08-13 07:15:43 +0530

    Refactor GlobalException class

--- src/main/java/com/stackroute/exceptions/GlobalException.java
+++ src/main/java/com/stackroute/exceptions/GlobalException.java
@@ -14,12 +14,16 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 //The@ControllerAdvice annotation allows us to consolidate our multiple, scattered @ExceptionHandlers
 // from before into a single, global error handling component.
 public class GlobalException extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(value
-            = {TrackAlreadyExistsException.class, TrackNotFoundException.class})
-    protected ResponseEntity<Object> handleConflict(
-            Exception ex, WebRequest request) {
-        String bodyOfResponse = "This class is for the global exception controller";
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+     @ExceptionHandler({TrackAlreadyExistsException.class})
+    public ResponseEntity<String> handleTrackAlreadyExistsException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({TrackNotFoundException.class})
+    public ResponseEntity<String> handleTrackNotFoundExceptions(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
     }
 }
 
